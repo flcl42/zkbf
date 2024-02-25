3ba1e61a1907640aa5faff84450b584b242d6e48
Alexey Osipov <me@flcl.me>
2024-02-25 13:39:07 +0300

    Just simple bf

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿// using System;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+
+        string program = args[0];
+        int length = program.Length;
+
+        byte[] data = new byte[1024];
+        int ptr = 0;
+        int skobki = 0;
+
+        for (int i = 0; i < length;)
+        {
+            switch (program[i])
+            {
+                case '>':
+                    ptr++;
+                    i++;
+                    break;
+                case '<':
+                    ptr--;
+                    if (ptr < 0)
+                    {
+                        return -1;
+                    }
+                    i++;
+                    break;
+
+                case '+':
+                    if (data[ptr] == 255)
+                    {
+                        return -1;
+                    }
+                    i++;
+                    data[ptr]++;
+                    break;
+                case '-':
+                    if (data[ptr] == 0)
+                    {
+                        return -1;
+                    }
+                    data[ptr]--;
+                    i++;
+                    break;
+
+                case '.':
+                    // Console.WriteLine(data[ptr]);
+                    // i++;
+                    // break;
+                    return -1;
+                case ',':
+                    // if(!int.TryParse(Console.ReadLine(), out int res) || res < 0 || res > 255){
+                    //     return -1;
+                    // };
+                    // data[ptr] = (byte)res;
+                    // i++;
+                    // break;
+                    return -1;
+
+                case '[':
+                    if (data[ptr] == 0)
+                    {
+                        skobki++;
+                        do
+                        {
+                            i++;
+                            if (i == program.Length)
+                            {
+                                return -1;
+                            }
+                            if (program[i] == '[')
+                            {
+                                skobki++;
+                            }
+                            if (program[i] == ']')
+                            {
+                                skobki--;
+                            }
+                        } while (skobki != 0);
+                    }
+                    i++;
+                    break;
+
+                case ']':
+                    if (data[ptr] != 0)
+                    {
+                        skobki++;
+                        do
+                        {
+                            if (i == 0)
+                            {
+                                return -1;
+                            }
+                            i--;
+                            if (program[i] == '[')
+                            {
+                                skobki--;
+                            }
+                            if (program[i] == ']')
+                            {
+                                skobki++;
+                            }
+                        } while (skobki != 0);
+                    }
+                    i++;
+                    break;
+            }
+        }
+
+        return data[ptr];
+    }
+}
---
+++ run.ps1
@@ -0,0 +1,4 @@
+bflat build -Os --stdlib:zero --no-pie --no-debug-info --arch x86
+
+bflat build -Os --stdlib:zero --no-pie --no-debug-info --arch x86 -c
+C:\Programs\crinkler23\Win64\Crinkler.exe .\zkbf.obj /out:zkbfc.exe /entry:__managed__Main $env:USERPROFILE\AppData\Local\Microsoft\WinGet\Packages\MichalStrehovsky.bflat_Microsoft.Winget.Source_8wekyb3d8bbwe\lib\windows\x86\zerolibnative.obj C:\Progra~2\WI3CF2~1\10\Lib\10.0.22621.0\um\x86\user32.lib C:\Progra~2\WI3CF2~1\10\Lib\10.0.22621.0\um\x86\kernel32.lib C:\Progra~2\WI3CF2~1\10\Lib\10.0.22621.0\um\x86\shell32.lib  /subsystem:windows
